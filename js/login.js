1d6e4e6fe900e625c2a90f0bbebe238494700206
antoninalangorte <88443675+antoninalangorte@users.noreply.github.com>
2021-08-22 20:20:04 -0300

    Update login.js

--- js/login.js
+++ js/login.js
@@ -3,7 +3,7 @@ function validar(){
     let nombre = document.getElementById("nombre").value;
     let pass = document.getElementById("pass").value;
     if ((nombre !== "") && (pass !== "")){
-        window.location.href="portada.html"
+        window.location.href="./portada.html"
     }
     else{
         alert ("Debe completar todos los campos");
